80a37b93a71dfc24802f93a20d4e9af3ea94cea9
dollarortwo <jleong001@gmail.com>
2026-05-13 10:08:11 -0700

    made changes to remove_task function. Added change_task function and applied it to the main function.

--- task_manager.py
+++ task_manager.py
@@ -1,31 +1,30 @@
 def display_menu():
     print("\t1. Add a task")
     print("\t2. Remove a task")
-    print("\t3. Edit a task")
+    print("\t3. Change a task")
     print("\t4. View all tasks")
     print("\t5. Quit")
 
-def add_task(list, item): 
-    list.append(item)
-    return list
+def add_task(tasks_list, item): 
+    tasks_list.append(item)
+
+def remove_task(tasks_list, item):
+    tasks_list.pop(item)
+
+def change_task(tasks_list, index, new_value):
+    index = index - 1
+    tasks_list[index] = new_value
     
-def remove_task(list, item):
-    list.remove(item)
-    return list
-
-def edit_task():
-    pass
-
-def view_task(list):
-    for index, task in enumerate(list):
-        print(f"{index}. {task}")
+def view_task(tasks_list):
+    for index, task in enumerate(tasks_list):
+        print(f"{index+1}. {task.capitalize()}")
 
 def quit_program():
     print("Goodbye!")
     
-    
 def main():
     print("Welcome to the Task Manager!\n")
+    my_tasks = []
     while True: 
         display_menu()
         try:
@@ -33,8 +32,6 @@ def main():
         except ValueError:
             print("Invalid input. Please try again")
             continue
-        
-        my_tasks = ["sleep", "write"]
         if number_selection == 1:
             try:
                 task = input("Enter a task: ")
@@ -49,13 +46,24 @@ def main():
             print("\nTask added.\n")
         elif number_selection == 2:
             try:
-                rm_task = input("Enter a task: ")
+                user_facing_task_number = int(input("Type in task number: "))
+                actual_index_list = user_facing_task_number - 1
             except ValueError:
                 print("Invalid input. Please try again")
                 continue
-            remove_task(my_tasks, task)
+            remove_task(my_tasks, actual_index_list)
+        elif number_selection == 3:
+            try:
+                task_selection_index = int(input("Type in a task number: "))
+                new_task = input("Please enter a new task: ")
+            except ValueError:
+                print("Invalid input. Please try again")
+                continue
+            change_task(my_tasks, task_selection_index, new_task)
         elif number_selection == 4:
             view_task(my_tasks)
+            if my_tasks == []:
+                print("No tasks")
         elif number_selection == 5:
             quit_program()
             break
